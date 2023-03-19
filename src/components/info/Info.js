77e9cde2226f190b5a73b9a83f836df17bc9ba29
Daniel <danydeitu18e@mail.com>
2023-03-18 23:26:23 -0300

    cambio paddin foott

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -6,7 +6,7 @@ const Info = () => {
     <div className="info-container">
       <div className="info">
         <h2>Realizo proyectos con Javascript Node Express Html Css React </h2>
-        <h3>Actualmente estoy estudiando Python Arduino Base de Datos Relacionalesy No Relacionales e Ingles</h3>
+        <h3>Tengo concimientos en Python, Arduino, C++, Base de Datos, MYSQL , Firebase, Mongo</h3>
       </div>
     </div>
   );
