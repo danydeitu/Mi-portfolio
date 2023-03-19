df093d2f3184f2819d401536788762dbae39e87b
Daniel <danydeitu18e@mail.com>
2023-03-19 04:57:30 -0300

    add proect

--- src/components/slider/Slides.jsx
+++ src/components/slider/Slides.jsx
@@ -6,38 +6,56 @@ const slidesInfo = [
   {
    
     src:
-      "/img/noCountryCinenema.jpg",
+      "/img/sol.jpg",
     alt: "Projecto 1",
+    desc: <a href="https://solylunacasadecampo.com/">Alquiler de Casa</a>
+  
+   
+  },
+  {
+   
+    src:
+      "/img/dany.jpg",
+    alt: "Projecto 2",
+    desc: <a href="https://www.disenowebdany.tech/">Agencia de Desarrollo web</a>
+  
+   
+  },
+  {
+   
+    src:
+      "/img/noCountryCinenema.jpg",
+    alt: "Projecto 3",
     desc: <a href="https://c4-g17-m.vercel.app/">App Cine NoCountry</a>
   
    
   },
-
-  {
-  src:
-  "/img/gifExpert.jpg",
-alt: "Projecto 2",
-desc: <a href="https://giff-expert.vercel.app/">App Giff Expert</a>
-},
-{
-  src:
-    "/img/app-tareas.jpg",
-  alt: "Projecto 3",
-  desc: <a href="https://tareas-app-topaz.vercel.app/">App Tares</a>
-},
-  
   {
     src:
       "/img/colmena.jpg",
     alt: "Projecto 4",
-    desc: <a href="https://github.com/danydeitu/Grupo3---C10/tree/develop">E-commerceColmena de Miel</a>
+    desc: <a href="https://github.com/danydeitu/Grupo3---C10/tree/develop">Tienda Productos Miel</a>
   },
   {
     src:
       "/img/mercadoLiebre.jpg",
     alt: "Projecto 5",
-    desc:  <a href="mercado-liebre2.vercel.app">E-commerce MercadoLiebre</a>,
+    desc:  <a href="mercado-liebre2.vercel.app">Tienda Mercado Tecno</a>,
   },
+  {
+  src:
+  "/img/gifExpert.jpg",
+alt: "Projecto 6",
+desc: <a href="https://giff-expert.vercel.app/">App Giff Expert</a>
+},
+{
+  src:
+    "/img/app-tareas.jpg",
+  alt: "Projecto 7",
+  desc: <a href="https://tareas-app-topaz.vercel.app/">App Tares</a>
+},
+  
+  
 
 
 ];
